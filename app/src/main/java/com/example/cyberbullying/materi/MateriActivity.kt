ef768c628bfd5aba2e0c07418431a2832bf73852
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-12-06 10:51:04 +0700

    add pdfviwer

--- app/src/main/java/com/example/cyberbullying/materi/MateriActivity.kt
+++ app/src/main/java/com/example/cyberbullying/materi/MateriActivity.kt
@@ -1,12 +1,22 @@
 package com.example.cyberbullying.materi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cyberbullying.R
+import com.example.cyberbullying.databinding.ActivityMateriBinding
 
 class MateriActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMateriBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_materi)
+        binding = ActivityMateriBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnDefinisi.setOnClickListener{
+            val i = Intent(this, PengertianActivity::class.java)
+            startActivity(i)
+        }
     }
 }
