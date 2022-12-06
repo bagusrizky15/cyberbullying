6f286a9ae656b3ae56ec2087b6b447545fee86d0
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-12-06 12:54:58 +0700

    .

---
+++ app/src/main/java/com/example/cyberbullying/materi/BentukActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cyberbullying.materi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BentukActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bentuk)
+    }
+}
---
+++ app/src/main/java/com/example/cyberbullying/materi/FaktorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cyberbullying.materi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FaktorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_faktor)
+    }
+}
