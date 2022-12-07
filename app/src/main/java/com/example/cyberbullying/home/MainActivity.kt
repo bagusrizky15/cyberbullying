9c43f4dfe44b67313d69c73991d5fc1ce08538c3
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-12-07 22:14:57 +0700

    add custom dialog

--- app/src/main/java/com/example/cyberbullying/home/MainActivity.kt
+++ app/src/main/java/com/example/cyberbullying/home/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.cyberbullying.home
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import com.example.cyberbullying.R
 import com.example.cyberbullying.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +24,23 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
         }
 
+        binding.btnExit.setOnClickListener{
+            customDialog()
+        }
+
     }
+
+    private fun customDialog(){
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setContentView(R.layout.custom_dialog)
+
+        val btnClose = dialog.findViewById<Button>(R.id.btn_close)
+        btnClose.setOnClickListener{
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
 }
